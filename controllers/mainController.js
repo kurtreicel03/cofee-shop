586f25ed11c7fcaf95b7ffd36f1b1e98be1bb96b
Kurt Reicel Nunez <kurtreicel@gmail.com>
2021-09-08 19:34:13 +0800

    add partials in template

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,8 @@
-exports.getIndex = (req,res)=>{
-    res.status(200).render('home')
-}
+exports.getIndex = (req, res) => {
+  const data = {
+    title: 'Cofee Shop',
+    description:
+      'Welcome to our house of coffee! We Hope you enjoy our broad selection of premium coffee from all over the world.',
+  };
+  res.status(200).render('home', data);
+};
